be35eeeefe346018f41a209e67bb730cb306ec9e
sedrainfo <taherhaouet@gmail.com>
2024-04-02 13:17:47 +0200

    rectif Titre

--- src/Components/PostDetail.tsx
+++ src/Components/PostDetail.tsx
@@ -5,7 +5,7 @@ postdetail : PostData | null
 const PostDetail : React.FC <postdetailProps> = ({postdetail}) =>  {
   return (
     <div>
-    <h2>Title : { postdetail?.title}</h2>
+    <h2>Titre : { postdetail?.title}</h2>
     <p> Body : {postdetail?.body}</p>
     
     </div>
